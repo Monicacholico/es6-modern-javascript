ab2219dd640e71810d665406108cfe4b7c22bb98
Monicacholico <monicachol@hotmail.com>
2020-08-28 23:14:38 -0500

    chapter 47

--- es6modules/app.js
+++ es6modules/app.js
@@ -3,11 +3,22 @@ import insane from 'insane';
 import jsonp from 'jsonp';
 import { apiKey as key, url, sayHi, old, dog } from './src/config.js';
 
-console.log(key, url);
-console.log(`The age of my dog ${dog} is ${old}`);
+// console.log(key, url);
+// console.log(`The age of my dog ${dog} is ${old}`);
 
-sayHi('Monica');
+// sayHi('Monica');
 
-const ages = [1,1,4, 52, 12, 4];
+// const ages = [1,1,4, 52, 12, 4];
 
-console.log(uniq(ages));
+// console.log(uniq(ages));
+
+import User, { createURL, gravatar} from './src/user';
+
+const monica = new User('Monica', 'monica@email.com', 'planeta-mujer.org');
+console.log(monica);
+
+const profile = createURL(monica.name);
+console.log(profile);
+const image = gravatar(monica.email);
+console.log(image);
+
