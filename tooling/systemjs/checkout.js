60fa896213a2ff419adddbe4dd7a0162f4d99691
Monicacholico <monicachol@hotmail.com>
2020-08-29 00:28:16 -0500

    chapter 48

---
+++ tooling/systemjs/checkout.js
@@ -0,0 +1,5 @@
+// import { sum, kebabCase} from 'lodash';
+// console.log(kebabCase('Hey this is cool!'));    
+import { greeting } from './main';
+
+console.log(greeting('Mon'));
---
+++ tooling/systemjs/main.js
@@ -0,0 +1,16 @@
+// import { sum } from 'npm:lodash';
+
+// console.log(kebabCase('Monica is sooo cool!'));
+
+// const numA = [3,4,5, 6,7];
+// const numb = [8, 9 , 10, 11, 24];
+
+// console.log(sum(numA, numb));
+
+console.log('hello world');
+
+export default function greeting (name) {
+    console.log(`hello ${name}`);
+}
+
+greeting('Monica');
